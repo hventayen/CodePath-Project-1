29f27f18da73675531cf3ff544db7e0adee81919
hventayen <harrold.ventayen@gmail.com>
2023-03-01 20:41:21 -0800

    Add files via upload

---
+++ mystory/mystory/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  mystory
+//
+//  Created by Harrold Ventayen on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button1tapped(_ sender: Any) {
+        
+    }
+    @IBAction func button2tapped(_ sender: Any) {
+        
+    }
+    @IBAction func button3tapped(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
